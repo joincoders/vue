2d8a048cec3f6f5634c259a1404e89362d45c8a8
JoinCoder <82702646+joincoders@users.noreply.github.com>
2021-12-29 16:05:48 +0700

    Install project CLR

---
+++ Project_1/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <h1>{{ project }}</h1>
+  <div v-if="showModal">
+    <Modal theme="sale" @closeform="toggleModal" >
+        <h1>Sign up for get giveaway</h1>
+        <p>Welcome to join with joincoder</p>
+
+        <template v-slot:links>
+            <a href="#">Sign up now </a>
+            <a href="#">More info</a>
+        </template>
+        
+    </Modal>
+  </div>
+
+  <button @click="toggleModal">Open Modal</button>
+</template>
+
+<script>
+
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App',
+  components:{
+    Modal
+  },
+  data() {
+    return {
+      project:'My first project',
+      header: 'Sign up to give away',
+      text: 'Welcom to joincoder',
+      showModal:false
+    }
+  },
+  methods:{
+    handleClick(){
+      console.log(this.$refs.project);
+      this.$refs.project.classList.add('active')
+      this.$refs.project.focus()
+    },
+    toggleModal(){
+      this.showModal =!this.showModal
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Project_1/src/components/Modal.vue
@@ -0,0 +1,68 @@
+<template>
+    <div class="backdrop" @click.self="closeModal" >
+        <div class="modal" :class="{sale: theme==='sale'}">
+            <slot></slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['theme'],
+    methods:{
+        closeModal(){
+            this.$emit('closeform');
+        }
+    }
+}
+</script>
+
+<style>
+    .modal.sale {
+        background:rgb(211, 125, 125);
+        color: #ffffff;
+    }
+        .modal.sale h1 {
+        color: white;
+    }
+
+    .modal {
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background:white;
+        border-radius: 10px;
+    }
+    .backdrop {
+        top:0;
+        position: fixed;
+        background: rgba(0, 0, 0, 5);
+        width: 100%;
+        height: 100%;
+
+    }
+    .modal .actions{
+        text-align: center;
+        margin:30px 0 10px 0 ;
+    }
+    .modal .actions a {
+        color: rgb(240, 219, 229);
+        padding: 8px;
+        border: 1px solid #eee;
+        border-radius:4px;
+        text-decoration: none;
+        margin:10px;
+
+    }
+    .modal h1{
+        color: burlywood;
+        border:none;
+        padding:0;
+    }
+    .modal p {
+        font-style: normal;
+    }
+</style>
